3ca8516fa3dde05703b7d53bc22c4a26c2890581
Nathan Benedetto Proença <nathanpro@haskell.linux.ime.usp.br>
2014-03-21 13:15:00 -0300

    Mais dois problemas

---
+++ URI/1189v01.cpp
@@ -0,0 +1,27 @@
+// http://www.urionlinejudge.com.br/judge/pt/problems/view/1183
+#define SIZE 6
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+        float M[SIZE][SIZE], ans;
+        char op;
+        int counter;
+
+        cin >> op;
+        ans = 0;
+
+        for( counter = 0 ; counter < SIZE*SIZE ; counter++ )
+                cin >> M[counter/SIZE][counter%SIZE];
+        
+        for( counter = 0 ; counter < SIZE*SIZE ; counter++ )
+                if( (counter/SIZE > counter%SIZE) && (counter/SIZE + counter%SIZE < SIZE-1) ){
+                        ans +=M[counter/SIZE][counter%SIZE];
+                }
+
+        if( op == 'M' ) ans /= 66;
+
+        printf("%.1f\n", ans);
+}
